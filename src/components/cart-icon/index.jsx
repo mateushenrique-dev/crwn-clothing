d9f806172255aed1059c084887d09b0dd43349a5
Mateus Henrique <mhenriqq32@gmail.com>
2022-01-15 11:27:22 -0300

    updating our aplicattion with memoizon selector

--- src/components/cart-icon/index.jsx
+++ src/components/cart-icon/index.jsx
@@ -1,18 +1,25 @@
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.scss";
 
-import { connect } from "react-redux"
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { connect } from "react-redux";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectorCartItemsCount } from "../../redux/cart/cart.selectors";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemQuantity }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemQuantity}</span>
   </div>
 );
+
+const mapStateToProps = (state) => {
+  return {
+    itemQuantity: selectorCartItemsCount(state),
+  };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
